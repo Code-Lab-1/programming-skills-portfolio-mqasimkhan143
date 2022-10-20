3988b21cbaf17957ab8135da9b82340c66188110
mqasimkhan143 <113086699+mqasimkhan143@users.noreply.github.com>
2022-10-20 10:23:36 +0400

    new codes

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 4.py
@@ -0,0 +1,5 @@
+##exercise 4:
+a = str(input())
+b = str(input())
+c = str(input())
+print(a,b,c)
---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 5.py
@@ -0,0 +1,4 @@
+##exercise 5:
+r = int(input())##enter int value
+pi = 3.14
+print(r*r*pi,"is the area of circle")
