eeeb738da102b1e2da0d2f091ab5166383b80cec
mqasimkhan143 <113086699+mqasimkhan143@users.noreply.github.com>
2022-11-22 10:20:22 +0400

    new codes

---
+++ Chapter 5- Dictionaries/Exercises/extra1.py
@@ -0,0 +1,4 @@
+dic1 = {"a":"56",
+        "b":"65"}
+dic1.update({"c":"82"})      
+print(dic1)  
---
+++ Chapter 5- Dictionaries/Exercises/extra4.py
@@ -0,0 +1,6 @@
+dict1 = {"x":"horizontal",
+         "y":"vertical",
+         "z":"diagonal"}
+print(dict1)
+dict1.pop("z")
+print(dict1) 
